31ac8ee109c9dc77db2f61479dd24170fe46d097
yangga <yangga0070@gmail.com>
2018-04-17 22:18:59 +0900

    chart/history

--- src/routes/chart/history.js
+++ src/routes/chart/history.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const CUG = require('cryptocurrency-udf-gateway');
 
-/* GET users listing. */
+const EMPTY_HISTORY = {t: [], o: [], h: [], l: [], c: [], v: [], s: 'ok'};
+
 router.get('/', function(req, res, next) {
-  res.json({hello:'world'})
+  const {
+    symbol,
+    resolution
+  } = req.query;
+
+  const param = new CUG.Chart.Helpers.HistoryParam({
+    symbol,
+    resolution
+  });
+
+  CUG.Chart.Helpers.history(param, function(err, result){
+    if (err){
+      res.json(EMPTY_HISTORY);
+    } else {
+      res.json(result);
+    }
+  })
 });
 
 module.exports = router;
